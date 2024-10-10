268528094b9a580a9847a0efc6035ed02c6e7ba4
Xinlana1 <julianaoaguilar16@gmail.com>
2024-10-09 22:52:30 -0300

    Debuggin

--- script.js
+++ script.js
@@ -1,55 +1,61 @@
-class Circulo {
-    constructor(diametro) {
-        this.diametro = diametro;
-        this.raio = this.diametro / 2
+class Circle { // create a class named "Circle"
+    constructor(diameter) {
+        this.diameter = diameter;
+        this.radius = this.diameter / 2
     }
-    circunferencia() {
-        let circunferencia = parseFloat((Math.PI * 2 * this.raio).toFixed(2));
-        return circunferencia;
+    circumference() {
+        let circumference = parseFloat((Math.PI * 2 * this.radius).toFixed(2));
+        return circumference;
     }
     area() {
-        let area = parseFloat((Math.PI * this.raio * this.raio).toFixed(2));
+        let area = parseFloat((Math.PI * this.radius * this.radius).toFixed(2));
         return area;
     }
-
-   
 };
 
-function criarCirculo() {
+function createCircle() {
+    let createCircle = document.getElementById("diameter").value;
+    let convertToFloat = parseFloat(createCircle);
+    let circle = new Circle(convertToFloat);
 
 
-    let criarCirculo = document.getElementById("diametro").value;
-    let converterParaNumero = parseFloat(criarCirculo);
-    let circulo = new Circulo(converterParaNumero);
+    document.getElementById("circle").innerHTML =
+        `raio: ${circle.radius},
+     area: ${circle.area()},
+      circunferencia: ${circle.circumference()}`
 
-    if (isNaN(circulo.area()) && isNaN(circulo.circunferencia()) && isNaN(circulo.raio)) {
-        document.getElementById("circulo").innerHTML = "Insira um valor válido!";
+    verifyAndAddValues();
+
+    function verifyAndAddValues() {
+
+        if (isNaN(circle.area()) && isNaN(circle.circumference()) && isNaN(circle.radius)) {
+            document.getElementById("circle").innerHTML = "Insira um valor válido!";
+            clearCalculations();
+        } else {
+            addCalculations();
+        }
+
+        function clearCalculations() {
+            document.getElementById("diameterLine").innerHTML = "";
+            document.getElementById("radiusLine").innerHTML = "";
+            document.getElementById("operations").innerHTML = "";
+        }
+
+        function addCalculations() {
+            document.getElementById("diameterLine").innerHTML = `Diametro (d) = ${circle.diameter}`;
+            document.getElementById("radiusLine").innerHTML = `Raio (r) = ${circle.radius}`;
+            document.getElementById("operations").innerHTML = `raio (r) = d / 2 <br>
+            r = ${circle.diameter} / 2 <br>
+             r = ${circle.radius} <br> <br>
+              area (a) = πr² <br>
+               a = π (prox. 3,14) * ${circle.radius}² <br>
+                a = ${circle.area()} <br> <br>  
+                circunferencia (C) = 2πr <br>
+                C = 2 * π * ${circle.radius} <br>
+                C = ${circle.circumference()}`;
+        }
     }
-
-    else {
-
-    document.getElementById("circulo").innerHTML =
-        `raio: ${circulo.raio},
-     area: ${circulo.area()},
-      circunferencia: ${circulo.circunferencia()}`
-
-      document.getElementById("line-d").innerHTML = `Diãmetro (d) = ${circulo.diametro}`
-      document.getElementById("line-r").innerHTML = `Raio (r) = ${circulo.raio}`
-
-  
-      document.getElementById("oi").innerHTML = `raio (r) = d / 2 <br>
-       r = ${circulo.diametro} / 2 <br>
-        r = ${circulo.raio} <br> <br>
-         area (a) = πr² <br>
-          a = π (prox. 3,14) * ${circulo.raio}² <br>
-           a = ${circulo.area()} <br> <br> 
-           
-           circunferencia (C) = 2πr <br>
-           C = 2 * π * ${circulo.raio} <br>
-           C = ${circulo.circunferencia()}`;
-
-
-}};
+};
 
 
 
